627910e2f01881f8ae5eb460a0335e9b12a603f4
Zura014 <zuragagnidze50@gmail.com>
2023-10-18 21:16:49 +0400

    123

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px'
+    },
+    extend: {
+      colors: {
+        bodyColor: '#555555',
+        mainColor:'rgba(138, 43, 226, 1)',
+      },
+    },
+  },
+  plugins: [],
+}
+
