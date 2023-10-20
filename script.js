da3e3c3349c63a3d4f45e3fa33afaa039c1d7be9
Zura014 <zuragagnidze50@gmail.com>
2023-10-21 00:24:14 +0400

    421

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-let searchInputH = document.querySelector("#searchInputH");
-let searchButton = document.querySelector("#searchButton");
-searchButton.addEventListener("click", () => {
-    if (searchInputH.classList.contains("hidden")){
-        searchInputH.classList.remove("hidden");
-    }else{
-        searchInputH.classList.add("hidden");
-    }
-})
+// let searchInputH = document.querySelector("#searchInputH");
+// let searchButton = document.querySelector("#searchButton");
+// searchButton.addEventListener("click", () => {
+//     if (searchInputH.classList.contains("hidden")){
+//         searchInputH.classList.remove("hidden");
+//     }else{
+//         searchInputH.classList.add("hidden");
+//     }
+// })
+
