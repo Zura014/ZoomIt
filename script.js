1e6346af2c9c75c851d89bf14feca137c1a36901
Zura014 <zuragagnidze50@gmail.com>
2023-10-20 23:02:10 +0400

    412

---
+++ script.js
@@ -0,0 +1,9 @@
+let searchInputH = document.querySelector("#searchInputH");
+let searchButton = document.querySelector("#searchButton");
+searchButton.addEventListener("click", () => {
+    if (searchInputH.classList.contains("hidden")){
+        searchInputH.classList.remove("hidden");
+    }else{
+        searchInputH.classList.add("hidden");
+    }
+})
