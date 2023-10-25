bbe8a9b6b8e04c035e9c167307f1973b0729f304
Zura014 <zuragagnidze50@gmail.com>
2023-10-25 18:57:36 +0400

    54

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px'
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
     },
-    width:{
-      78: '300px',
-      2324:'100%',
-      82:'324px',
-      84: '84px'
+    width: {
+      78: "300px",
+      2324: "100%",
+      82: "324px",
+      84: "84px",
     },
-    height:{
-      505: '505px',
-      2334:'100%',
-      82:'324px',
-      68:'68px'
+    height: {
+      505: "505px",
+      2334: "100%",
+      82: "324px",
+      68: "68px",
     },
     extend: {
       colors: {
-        bodyColor: '#f5f5f5',
-        categoriesColor: '#dabfe1',
-        categoriesSvgColor: '#bcd9e4',
-        titleColor: '#21225f',
-        activeColor: '#8a2be2',
-        buttonColor: '#8000a3',
-        subjectColor: '#08a9e61a',
-        borderColor: 'rgba(255, 255, 255, .1)',
+        bodyColor: "#f5f5f5",
+        categoriesColor: "#dabfe1",
+        categoriesSvgColor: "#bcd9e4",
+        titleColor: "#21225f",
+        activeColor: "#8a2be2",
+        buttonColor: "#8000a3",
+        subjectColor: "#08a9e61a",
+        borderColor: "rgba(255, 255, 255, .1)",
       },
       fontFamily: {
-        roboto: ['Noto Sans Georgian', 'sans-serif'],
+        roboto: ["Noto Sans Georgian", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
-
+};
