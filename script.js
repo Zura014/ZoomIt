80d327057ca08079d721fec9a8132026fb70c09f
Zura014 <zuragagnidze50@gmail.com>
2023-10-29 00:34:24 +0400

    workingMenu

--- script.js
+++ script.js
@@ -7,20 +7,36 @@ let searchMenu = document.querySelector("#searchMenu");
 let pagesButton = document.querySelector("#pagesButton");
 let pagesMenu = document.querySelector("#pagesMenu");
 
-pagesButton.addEventListener("mouseover", () => {
-    pagesButton.classList.toggle("open");
-    pagesMenu.classList.toggle("scale-1");
-    pagesMenu.classList.toggle("scale-0");
+let hoverBtn = document.querySelector("#hoverBtn");
+
+pagesButton.addEventListener("mouseover", function () {
+    pagesMenu.classList.add("scale-y-1");
+    pagesMenu.classList.remove("scale-y-0");
+    if (pagesMenu.classList.contains("scale-y-1")) {
+        hoverBtn.classList.add("text-activeColor");
+    } else {
+        hoverBtn.classList.remove("text-activeColor");
+    }
 });
 
-searchMenuButton.addEventListener("click", () =>{
+pagesButton.addEventListener("mouseout", function () {
+    pagesMenu.classList.remove("scale-y-1");
+    pagesMenu.classList.add("scale-y-0");
+    if (pagesMenu.classList.contains("scale-y-1")) {
+        hoverBtn.classList.add("text-activeColor");
+    } else {
+        hoverBtn.classList.remove("text-activeColor");
+    }
+});
+
+searchMenuButton.addEventListener("click", () => {
     searchMenuButton.classList.toggle("open");
-    searchMenu.classList.toggle("scale-1")
-    searchMenu.classList.toggle("scale-0")
+    searchMenu.classList.toggle("scale-y-1");
+    searchMenu.classList.toggle("scale-y-0");
 });
 
 burgerMenuButton.addEventListener("click", () => {
     burgerMenuButton.classList.toggle("open");
-    burgerMenu.classList.toggle("scale-1")
-    burgerMenu.classList.toggle("scale-0")
+    burgerMenu.classList.toggle("scale-1");
+    burgerMenu.classList.toggle("scale-0");
 });
