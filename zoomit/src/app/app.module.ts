922a364b10de9f0472670f0be9e95846ee2ee803
Zura014 <zuragagnidze50@gmail.com>
2023-11-01 20:14:48 +0400

    picturesDone

--- zoomit/src/app/app.module.ts
+++ zoomit/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { FiltrationComponent } from './filtration/filtration.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { Grid1Component } from './grid1/grid1.component';
+import { Grid2Component } from './grid2/grid2.component';
+import { Grid3Component } from './grid3/grid3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    FiltrationComponent,
+    CategoriesComponent,
+    Grid1Component,
+    Grid2Component,
+    Grid3Component
   ],
   imports: [
     BrowserModule,
---
+++ zoomit/tailwind.config.js
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    width: {
+      78: "300px",
+      2324: "100%",
+      82: "324px",
+      84: "84px",
+      296: "296px"
+    },
+    height: {
+      505: "505px",
+      2334: "100%",
+      82: "324px",
+      68: "68px",
+      
+    },
+    extend: {
+      colors: {
+        bodyColor: "#f5f5f5",
+        categoriesColor: "#dabfe1",
+        categoriesSvgColor: "#bcd9e4",
+        titleColor: "#21225f",
+        activeColor: "#8a2be2",
+        buttonColor: "#8000a3",
+        subjectColor: "#08a9e61a",
+        borderColor: "rgba(255, 255, 255, .1)",
+        messengerIconColor: "#0A7CFF",
+        inputColor: "#86b7fe"
+      },
+      fontFamily: {
+        roboto: ["Noto Sans Georgian", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
